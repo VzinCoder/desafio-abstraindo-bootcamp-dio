647e7b1e5095558193a1e949a37f836b752b4c85
VzinCoder <contato.viniciusp.b@gmail.com>
2024-05-05 16:48:46 -0300

    adicionado classes bootcamp e dev

---
+++ src/dominio/Bootcamp.java
@@ -0,0 +1,122 @@
+package dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+
+    private String nome;
+    private String desricao;
+    private final LocalDate dataInicial;
+    private final LocalDate dataFinal;
+    private Set<Dev> devsIncritos;
+    private Set<Conteudo> conteudos;
+
+    public Bootcamp(String nome,String descricao) {
+        this.nome = nome;
+        this.desricao =  descricao;
+        this.dataInicial = LocalDate.now();
+        this.dataFinal = dataInicial.plusDays(45);
+        this.devsIncritos = new HashSet<>();
+        this.conteudos = new LinkedHashSet<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDesricao() {
+        return desricao;
+    }
+
+    public void setDesricao(String desricao) {
+        this.desricao = desricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public Set<Dev> getDevsIncritos() {
+        return devsIncritos;
+    }
+
+    public void setDevsIncritos(Set<Dev> devsIncritos) {
+        this.devsIncritos = devsIncritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((desricao == null) ? 0 : desricao.hashCode());
+        result = prime * result + ((dataInicial == null) ? 0 : dataInicial.hashCode());
+        result = prime * result + ((dataFinal == null) ? 0 : dataFinal.hashCode());
+        result = prime * result + ((devsIncritos == null) ? 0 : devsIncritos.hashCode());
+        result = prime * result + ((conteudos == null) ? 0 : conteudos.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Bootcamp other = (Bootcamp) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (desricao == null) {
+            if (other.desricao != null)
+                return false;
+        } else if (!desricao.equals(other.desricao))
+            return false;
+        if (dataInicial == null) {
+            if (other.dataInicial != null)
+                return false;
+        } else if (!dataInicial.equals(other.dataInicial))
+            return false;
+        if (dataFinal == null) {
+            if (other.dataFinal != null)
+                return false;
+        } else if (!dataFinal.equals(other.dataFinal))
+            return false;
+        if (devsIncritos == null) {
+            if (other.devsIncritos != null)
+                return false;
+        } else if (!devsIncritos.equals(other.devsIncritos))
+            return false;
+        if (conteudos == null) {
+            if (other.conteudos != null)
+                return false;
+        } else if (!conteudos.equals(other.conteudos))
+            return false;
+        return true;
+    }
+
+    
+}
---
+++ src/dominio/Dev.java
@@ -0,0 +1,74 @@
+package dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos;
+    private Set<Conteudo> conteudosConcluidos;
+
+    public Dev(String nome) {
+        conteudosInscritos = new LinkedHashSet<>();
+        conteudosConcluidos = new LinkedHashSet<>();
+    }
+
+    public void inscrever(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsIncritos().add(this);
+    }
+
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+         System.err.println("Voce nao esta matriculado em nenhum conteudo!");
+        }
+
+    }
+
+    public double calcularTotalXp() {
+     return  this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((conteudosInscritos == null) ? 0 : conteudosInscritos.hashCode());
+        result = prime * result + ((conteudosConcluidos == null) ? 0 : conteudosConcluidos.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dev other = (Dev) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (conteudosInscritos == null) {
+            if (other.conteudosInscritos != null)
+                return false;
+        } else if (!conteudosInscritos.equals(other.conteudosInscritos))
+            return false;
+        if (conteudosConcluidos == null) {
+            if (other.conteudosConcluidos != null)
+                return false;
+        } else if (!conteudosConcluidos.equals(other.conteudosConcluidos))
+            return false;
+        return true;
+    }
+
+}
