ef5fe37950a3168dd23236c43e5aa07ce6240b06
VzinCoder <contato.viniciusp.b@gmail.com>
2024-05-05 14:55:44 -0300

    adicionado as classes mentoria e curso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.time.LocalDate;
+
+import dominio.Curso;
+import dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Curso curso1 = new Curso("Curso java","descricao curso java",8);
+       Curso curso2 = new Curso("js", "descricao js", 4);
+
+       Mentoria mentoria = new Mentoria("mentoria java", "descricao mentoria", LocalDate.now());
+
+       System.out.println(mentoria);
+
     }
 }
