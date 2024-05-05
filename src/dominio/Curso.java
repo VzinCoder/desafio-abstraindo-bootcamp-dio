b9d7f8cdf794afcbe7965f07dea480d98554c35f
VzinCoder <contato.viniciusp.b@gmail.com>
2024-05-05 16:12:56 -0300

    adicionado classe conteudo e implementado a heranca

--- src/dominio/Curso.java
+++ src/dominio/Curso.java
@@ -2,32 +2,21 @@ package dominio;
 
 
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo{
     private int cargaHoraria;
 
+    @Override
+    public double calcularXp() {
+        
+        return XP_PADRAO*cargaHoraria;
+    }
+
+
     public Curso(String titulo,String descricao,int cargaHoraria){
-        this.titulo = titulo;
-        this.descricao = descricao;
+        super(titulo,descricao);
         this.cargaHoraria = cargaHoraria;
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -39,7 +28,7 @@ public class Curso {
 
     @Override
     public String toString() {
-        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+        return "Curso [titulo=" + super.getTitulo() + ", descricao=" + super.getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
     }
 
     
